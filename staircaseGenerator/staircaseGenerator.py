cc3a218bddaeff8e592c77e668dcf077f529e204
Cathy Chen <cathycat0131@gmail.com>
2022-07-11 16:15:06 -0700

    updated stair project with new slider and material assigning

--- staircaseGenerator/staircaseGenerator.py
+++ staircaseGenerator/staircaseGenerator.py
@@ -4,8 +4,11 @@ from maya import cmds
 import os
 import stairsClass
 from stairsClass import Stairs
+from maya import OpenMayaUI as omui
+from PySide2 import QtWidgets, QtGui, QtCore
 importlib.reload(stairsClass)
 
+cmds.file(force=True, newFile=True)
 USERAPPDIR = cmds.internalVar(userAppDir=True)
 DIRECTORY = os.path.join(USERAPPDIR, 'Stairs')
 
@@ -13,12 +16,13 @@ def createDirectory(directory=DIRECTORY):
     if not os.path.exists(directory):
         os.mkdir(directory)
 
-class staircaseGeneratorUI(object):
+class staircaseGeneratorUI(QtWidgets.QWidget):
     '''
     import staircaseGenerator as s
     importlib.reload(s)
     s.staircaseGeneratorUI().show()
     '''
+
     windowName = "Stairs Generator"
 
     def __init__(self):
@@ -26,7 +30,7 @@ class staircaseGeneratorUI(object):
         self.textField = None
 
     def show(self):
-        if cmds.window(self.windowName, query=True, exists=True):
+        if cmds.window(self.windowName, query=True, exists=True, width=800, height=100):
             cmds.deleteUI(self.windowName)
 
         cmds.window(self.windowName)
@@ -36,7 +40,7 @@ class staircaseGeneratorUI(object):
         cmds.showWindow()
 
     def buildUI(self):
-        column = cmds.columnLayout()
+        column = cmds.columnLayout(columnAttach=('left',10))
         cmds.rowLayout(numberOfColumns=2)
         self.textfield = pm.textField()
         #name = cmds.textField(self.textfield, q=True, text=True)
@@ -44,41 +48,140 @@ class staircaseGeneratorUI(object):
 
         cmds.setParent(column)
         cmds.separator(style='double', height=6)
-        cmds.text(label="Use the slider to modify the size of the stair")
-        cmds.rowLayout(numberOfColumns=4)
-        self.label = cmds.text(label="10")
-        self.slider = cmds.intSlider(min=2, max=15, value=10, step=1, dragCommand=self.modifyStairs)
-        cmds.button(label="Make stairs", command=self.makeStairs)
-        cmds.button(label="Reset", command=self.reset)
+
+        #  # This will be our button to display the color of the light
+        # self.colourBtn = QtWidgets.QPushButton()
+        # self.colourBtn.setMaximumWidth(20)
+        # self.colourBtn.setMaximumHeight(20)
+        # # Finally we call a method to sat the buttons color based on the lights current color
+        # self.setButtonColour()
+        # self.colourBtn.clicked.connect(self.setColour)
+
+        #self.colour = cmds.colorInputWidgetGrp( label='Color', rgb=(1, 0, 0),columnAlign=[1,'left'] )
+        # cmds.setParent(column)
+        # self.colour = cmds.colorSliderGrp( rgb=(0, 0, 1),columnAlign=[1,'left'],dragCommand=self.setColour())
 
         cmds.setParent(column)
-        cmds.text(label="Use the slider to modify the gap of the stair")
+        cmds.separator(style='double', height=6)
+        cmds.text(label="Modify the size of the stairs")
         cmds.rowLayout(numberOfColumns=2)
-        self.labelGap = cmds.text(label="0")
-        self.sliderGap = cmds.intSlider(min=0, max=15, value=0, step=1, dragCommand=self.modifyGap)
+        self.labelSize = cmds.text(label="1")
+        self.sliderSize = cmds.intSlider(min=1, max=10, value=1, step=1, dragCommand=self.makeStairs)
+        #cmds.button(label="Add size", command=self.modifySize)
+        #cmds.button(label="Reset", command=self.reset)
 
         cmds.setParent(column)
-        cmds.rowLayout(numberOfColumns=3)
-        cmds.button(label="Screenshot", command=self.saveScreenshot)
-        cmds.button(label="Close", command=self.close)
-        cmds.button(label="Delete", command=self.delete)
+        cmds.separator(style='double', height=6)
+        cmds.text(label="Modify the divisions of the stairs")
+        cmds.rowLayout(numberOfColumns=2)
+        self.labelDivision = cmds.text(label="10")
+        self.sliderDivision = cmds.intSlider(min=2, max=15, value=10, step=1, dragCommand=self.makeStairs)
+        #cmds.button(label="Add division", command=self.modifyDivision)
+        #cmds.button(label="Reset", command=self.reset)
 
-    def modifyStairs(self, size):
-        cmds.text(self.label, edit=True,label=str(size))
+        cmds.setParent(column)
+        cmds.separator(style='double', height=6)
+        cmds.text(label="Modify the elevation of the stairs")
+        cmds.rowLayout(numberOfColumns=2)
+        self.labelGap = cmds.text(label="1")
+        self.sliderGap = cmds.intSlider(min=1, max=15, value=1, step=1, dragCommand=self.makeStairs)
+        #cmds.button(label="Add gap", command=self.modifyGap)
+
+        cmds.setParent(column)
+        cmds.separator(style='double', height=6)
+        cmds.text(label="Modify the height of the stairs")
+        cmds.rowLayout(numberOfColumns=2)
+        self.labelHeight = cmds.text(label="1")
+        self.sliderHeight = cmds.intSlider(min=1, max=10, value=1, step=1, dragCommand=self.makeStairs)
+        #cmds.button(label="Add gap", command=self.modifyGap)
+
+        cmds.setParent(column)
+        cmds.separator(style='double', height=6)
+        cmds.rowLayout(numberOfColumns=4, columnAlign4=('right','right','right','right'))
+        cmds.button(label="standard", command=self.applyTexture_S)
+        cmds.button(label="lambert", command=self.applyTexture_L)
+        cmds.button(label="phong", command=self.applyTexture_P)
+        cmds.button(label="blinn", command=self.applyTexture_B)
+
+        cmds.setParent(column)
+        cmds.separator(style='double', height=6)
+        cmds.rowLayout(numberOfColumns=4, columnAlign4=('right','right','right','right'))
+        self.createButton = cmds.button(label="Create", command=self.makeStairs)
+        cmds.button(label="Screenshot", command=self.saveScreenshot)
+        cmds.button(label="Delete", command=self.delete)
+        cmds.button(label="Close", command=self.close)
+
+
+    def makeStairs(self, *args):
+        self.modifyText()
+        gap = cmds.intSlider(self.sliderGap, query=True, value=True)
+        size = cmds.intSlider(self.sliderSize, query=True, value=True)
+        division = cmds.intSlider(self.sliderDivision, query=True, value=True)
+        if self.stairs:
+            self.delete()
+        if not self.stairs:
+            self.stairs = Stairs()
+            self.stairs.currentDivision = division
+            self.stairs.currentSize = size
+            self.stairs.currentGap = gap
+            self.modifyDimension()
+            self.stairs.createStairs(size=size,division=division,gap=gap)
+
+    def modifyDivision(self, *args):
+        self.modifyText()
+        division = cmds.intSlider(self.sliderDivision, query=True, value=True)
+        if self.stairs:
+            self.stairs.changeDivision(division=division)
+
+    def modifyGap(self, *args):
+        self.modifyText()
+        gap = cmds.intSlider(self.sliderGap, query=True, value=True)
+
+        if self.stairs:
+            self.stairs.changeGap(gap=gap)
+
+    def modifyDimension(self, *args):
+        height = cmds.intSlider(self.sliderHeight, query=True, value=True)
+        cmds.text(self.labelHeight, edit=True,label=str(height))
+
+        # width = cmds.intSlider(self.sliderWidth, query=True, value=True)
+        # cmds.text(self.labelWidth, edit=True,label=str(width))
+        #
+        # depth = cmds.intSlider(self.sliderDepth, query=True, value=True)
+        # cmds.text(self.labelDepth, edit=True,label=str(depth))
+
+        if self.stairs:
+            self.stairs.changeHeight(height)
+            # self.stairs.changeWidth(width)
+            # self.stairs.changeDepth(depth)
+            #self.refreshModel()
+
+
+    def modifySize(self, *args):
+        self.modifyText()
+        size = cmds.intSlider(self.sliderSize, query=True, value=True)
         if self.stairs:
             self.stairs.changeSize(size=size)
 
-    def makeStairs(self, *args):
-        # We first need to see what the slider is set to, to find how many teeth we need to make
-        size = cmds.intSlider(self.slider, query=True, value=True)
-        self.stairs = Stairs()
-        self.stairs.currentSize = size
-        self.stairs.createStairs(size=size)
+    def modifyText(self, *args):
+        size = cmds.intSlider(self.sliderSize, query=True, value=True)
+        cmds.text(self.labelSize, edit=True,label=str(size))
 
-    def modifyGap(self, gap):
+        gap = cmds.intSlider(self.sliderGap, query=True, value=True)
         cmds.text(self.labelGap, edit=True,label=str(gap))
-        if self.stairs:
-            self.stairs.changeGap(gap=gap)
+
+        division = cmds.intSlider(self.sliderDivision, query=True, value=True)
+        cmds.text(self.labelDivision, edit=True,label=str(division))
+
+        height = cmds.intSlider(self.sliderHeight, query=True, value=True)
+        cmds.text(self.labelHeight, edit=True,label=str(height))
+
+        # width = cmds.intSlider(self.sliderWidth, query=True, value=True)
+        # cmds.text(self.labelWidth, edit=True,label=str(width))
+        #
+        # depth = cmds.intSlider(self.sliderDepth, query=True, value=True)
+        # cmds.text(self.labelDepth, edit=True,label=str(depth))
+
 
     def reset(self, *args):
         self.stairs = None
@@ -91,11 +194,13 @@ class staircaseGeneratorUI(object):
 
     def delete(self, *args):
         if self.stairs:
-            self.stairs.deleteStairs()
+            self.stairs.deleteAll()
+            self.stairs = None
         else: print("You need to make a stair first.")
 
     def saveScreenshot(self, *args):
-        self.stairs.saveScreenshot(name="Stairs")
+        if self.stairs:
+            self.stairs.saveScreenshot(name="Stairs")
 
     def saveFile(self, name, directory=DIRECTORY):
         createDirectory(directory)
@@ -107,10 +212,85 @@ class staircaseGeneratorUI(object):
         else:
             cmds.file(save=True,type="mayaAscii",force=True)
 
+    def refreshModel(self):
+        self.stairs.transformDimension()
+
     def save(self, *args):
         createDirectory()
         name = self.textfield.getText()
         print(name)
         self.saveFile(name=name)
 
+    def setColour(self,*args):
+        if self.stairs:
+            self.stairs.colour = cmds.colorSliderGrp(self.colour, query=True, value=True)
 
+
+
+    def setButtonColour(self, colour=None):
+        if self.stairs:
+            if not colour:
+                    colour = self.stairs.colour.get()
+            assert len(colour) == 3, "You must provide a list of 3 colours"
+
+            r,g,b = [c*255 for c in colour]
+            self.colourBtn.setStyleSheet('background-color: rgba(%s, %s, %s, 1.0)' % (r,g,b))
+
+    def applyTexture_L(self, type):
+        if self.stairs:
+            self.stairs.applyMaterial(type='lambert')
+
+    def applyTexture_S(self, type):
+        if self.stairs:
+            self.stairs.applyMaterial(type="standardSurface")
+
+    def applyTexture_B(self, type):
+        if self.stairs:
+            self.stairs.applyMaterial(type="blinn")
+
+    def applyTexture_P(self, type):
+        if self.stairs:
+            self.stairs.applyMaterial(type="phong")
+
+
+    def create_shader(name, node_type="lambert"):
+        material = cmds.shadingNode(node_type, name=name, asShader=True)
+        sg = cmds.sets(name="%sSG" % name, empty=True, renderable=True, noSurfaceShader=True)
+        cmds.connectAttr("%s.outColor" % material, "%s.surfaceShader" % sg)
+        return material, sg
+
+def getDock(name='LightingManagerDock'):
+    """
+    This function creates a dock with the given name.
+    It's an example of how we can mix Maya's UI elements with Qt elements
+    Args:
+        name: The name of the dock to create
+    Returns:
+        QtWidget.QWidget: The dock's widget
+    """
+    # First lets delete any conflicting docks
+    deleteDock(name)
+
+    # Then we create a workspaceControl dock using Maya's UI tools
+    # This gives us back the name of the dock created
+    ctrl = pm.workspaceControl(name, dockToMainWindow=('right', 1), label="Stairs Generator")
+
+     # We can use the OpenMayaUI API to get the actual Qt widget associated with the name
+    qtCtrl = omui.MQtUtil_findControl(ctrl)
+
+     # Finally we use wrapInstance to convert it to something Python can understand, in this case a QWidget
+    ptr = wrapInstance(Qt.long(qtCtrl), QtWidgets.QWidget)
+
+    # And we return that QWidget back to whoever wants it.
+    return ptr
+
+def deleteDock(name="Stairs Generator"):
+    """
+    A simple function to delete the given dock
+    Args:
+        name: the name of the dock
+    """
+    # We use the workspaceControl to see if the dock exists
+    if pm.workspaceControl(name, query=True, exists=True):
+         # If it does we delete it
+        pm.deleteUI(name)
--- staircaseGenerator/stairsClass.py
+++ staircaseGenerator/stairsClass.py
@@ -2,6 +2,7 @@ from maya import cmds
 import pymel.core as pm
 import os
 import maya.OpenMaya as OpenMaya
+from PySide2 import QtWidgets, QtGui, QtCore
 
 USERAPPDIR = cmds.internalVar(userAppDir=True)
 DIRECTORY = os.path.join(USERAPPDIR, 'Stairs')
@@ -10,10 +11,17 @@ def createDirectory(directory=DIRECTORY):
     if not os.path.exists(directory):
         os.mkdir(directory)
 
-class Stairs(object):
+class Stairs(QtWidgets.QWidget):
 
     face = []
-    currentSize = 0
+    created = False
+    cubeCreated = False
+    currentDivision = 10
+    currentSize = 1
+    currentGap = 1
+    height = 0.3
+    width = 1
+    depth = 1
 
     def __init__(self):
         #The _init_ method lets us set default values
@@ -22,32 +30,48 @@ class Stairs(object):
         self.shape = None
         self.extrude = None
         self.transform = None
+        self.face = None
+        self.cubeCreated = False
+        self.currentDivision = 10
+        self.currentSize = 1
+        self.currentGap = 1
+        self.height = 0.3
+        self.width = 1
+        self.depth = 1
+        self.colour = None
 
-    def createStairs(self,size=10, gap=1):
-        step = size - 1
+    def createStairs(self, size=currentSize, division=currentDivision, gap=currentGap):
+        step = division - 1
         counter = 0
         amount = 1
         extrudeAmount = 0
         complete = False
+        self.created = True
         i = 0
         direction = 0
         count = 0
         face = []
 
         #Create plane
-        transform, constructor = cmds.polyPlane(subdivisionsHeight=size, subdivisionsWidth=size)
+        transform, constructor = cmds.polyPlane(subdivisionsHeight=division, subdivisionsWidth=division)
+        cmds.setAttr('%s.sx' % transform, size);
+        cmds.setAttr('%s.sz' % transform, size);
         self.transform = transform
+        self.currentSize = size
+        self.currentDivision = division
+        self.currentGap = gap
+
         while(not complete):
             cmds.select(clear=True)
             if counter <= step:
                 if direction == 0:
                     amount = 1
                 elif direction == 1:
-                    amount = size
+                    amount = division
                 elif direction == 2:
                     amount = -1
                 elif direction == 3:
-                    amount = -size
+                    amount = -division
                 counter += 1
                 cmds.select('%s.f[%s]' % (transform, i))
                 i += amount
@@ -64,7 +88,7 @@ class Stairs(object):
                     direction = 0
                 else:
                     direction += 1
-                if step == size - 1:
+                if step == division - 1:
                     step -= 1
                 elif count == 0:
                     count += 1
@@ -73,8 +97,10 @@ class Stairs(object):
                     step -= 1
                 continue
 
-        self.outsideToInside(face, transform, size, gap)
+        self.outsideToInside(face, transform, division, gap)
+        #self.insideToOutside(face, transform, division, gap)
         cmds.delete(transform, constructor)
+        self.face = face
         return face
 
     def findCenterCoordinate(self,transform, index):
@@ -83,31 +109,61 @@ class Stairs(object):
         centerPoint = pm.datatypes.Point(pt)
         return centerPoint
 
-    def createCube(self, index, amount, centerPoint, size):
-        cmds.polyCube(n="cube%s" % index, height=0.01*1/size, width=1/size,
-                                             depth=1/size)
-        cmds.setAttr('%s.translateX' % ("cube%s" % index), centerPoint[0])
-        cmds.setAttr('%s.translateZ' % ("cube%s" % index), centerPoint[2])
+    def createCube(self, index, centerPoint, division=currentDivision, amount=0):
+        if self.cubeCreated:
+            cmds.polyCube(n="cube%s" % index, height=self.currentSize * 0.2 * self.height * (self.currentSize / self.currentDivision), width= self.currentSize * self.width * (self.currentSize / self.currentDivision),
+                                             depth=self.currentSize * self.depth * (self.currentSize / self.currentDivision), edit=True)
+        else:
+            cmds.polyCube(n="cube%s" % index, height=self.currentSize * 0.2 * self.height * (self.currentSize / self.currentDivision), width= self.currentSize * self.width * (self.currentSize / self.currentDivision),
+                                             depth=self.currentSize * self.depth * (self.currentSize / self.currentDivision))
+        #cmds.setAttr('%s.sx' % ("cube%s" % index), self.currentSize)
+        #cmds.setAttr('%s.sz' % ("cube%s" % index), self.currentSize)
+        cmds.setAttr('%s.translateX' % ("cube%s" % index), self.currentSize * centerPoint[0])
+        cmds.setAttr('%s.translateZ' % ("cube%s" % index), self.currentSize * centerPoint[2])
+        cmds.setAttr("cube%s.visibility" % index, 0)
+        #cmds.hide("cube%s" % index)
+
+    def changeDivision(self, division):
+        if self.created:
+            self.deleteAll()
+        self.currentDivision = division
+        self.createStairs(division=division)
 
     def changeSize(self, size):
-        for i in range(self.currentSize * self.currentSize):
-            cmds.delete('cube%s'% i)
-        self.createStairs(size=size)
+        if self.created:
+            self.deleteAll()
         self.currentSize = size
+        self.createStairs(size=size)
 
     def changeGap(self, gap):
-        for i in range(self.currentSize * self.currentSize):
-            cmds.delete('cube%s'% i)
-        self.createStairs(size=self.currentSize, gap=gap)
+        if self.created:
+            self.deleteAll()
+        self.currentGap = gap
+        self.createStairs(gap=gap)
 
+    def changeHeight(self, height):
+        self.height = height
+
+    def changeWidth(self, width):
+        self.width = width
+
+    def changeDepth(self, depth):
+        self.depth = depth
 
     def transformCube(self, index, amount, centerPoint, gap):
         if amount == 0:
             amount = 0.5
-        cmds.setAttr('%s.sy' % ("cube%s" % index), amount*5)
-        cmds.setAttr('%s.translateY' % ("cube%s" % index), (gap * (amount*0.005/2)))
+        #cmds.setAttr('%s.sy' % ("cube%s" % index), amount*5)
+        cmds.setAttr('%s.translateY' % ("cube%s" % index), ( 0.01 * self.height * self.currentSize * gap * self.currentSize * amount))
 
-    def outsideToInside(self, face, transform, size, gap):
+    def transformDimension(self):
+        #Transform Cubes
+        self.deleteStairs()
+        self.outsideToInside(self.face, self.transform, self.currentDivision, self.currentGap)
+
+
+
+    def outsideToInside(self, face, transform, division, gap):
         count = 1
         counter = 1
         frame = 1
@@ -115,33 +171,74 @@ class Stairs(object):
         #Create cubes
         for i in face:
             centerPoint = self.findCenterCoordinate(transform, i)
-            self.createCube(i, count, centerPoint, size)
+            self.createCube(i, centerPoint)
             cmds.setKeyframe('%s.translateY' % ("cube%s" % i), t=1)
             cmds.setKeyframe('%s.sy' % ("cube%s" % i), t=1)
-            cmds.autoKeyframe(edit=True)
+            cmds.setKeyframe("cube%s.visibility" % i, t=1)
+
 
         #Transform Cubes
         for i in range(len(face)):
             centerPoint = self.findCenterCoordinate(transform, face[i])
             self.transformCube(face[i], i, centerPoint, gap)
+            cmds.setAttr("cube%s.visibility" % face[i], 1)
             cmds.setKeyframe('%s.translateY' % ("cube%s" % face[i]), t=frame)
             cmds.setKeyframe('%s.sy' % ("cube%s" % face[i]), t=frame)
-            if i < size*size-1:
+            cmds.setKeyframe("cube%s.visibility" % face[i], t=frame)
+            if i < division*division-1:
                 cmds.setKeyframe('%s.translateY' % ("cube%s" % face[i+1]), t=frame)
                 cmds.setKeyframe('%s.sy' % ("cube%s" % face[i+1]), t=frame)
+                cmds.setKeyframe("cube%s.visibility" % face[i+1], t=frame)
+            frame += 1
+            # if count == size or count == 0:
+            #     counter = -1*counter
+            count += counter
+        self.cubeCreated = True
+
+    def insideToOutside(self, face, transform, division, gap):
+        count = 1
+        counter = 1
+        frame = 1
+
+        #Create cubes
+        for i in face:
+            centerPoint = self.findCenterCoordinate(transform, i)
+            self.createCube(i, count, centerPoint)
+            cmds.setKeyframe('%s.translateY' % ("cube%s" % i), t=1)
+            #cmds.setKeyframe('%s.sy' % ("cube%s" % i), t=1)
+            cmds.autoKeyframe(edit=True)
+
+        #Transform Cubes
+        for i in range(len(face)-1, -1, -1):
+            centerPoint = self.findCenterCoordinate(transform, face[i])
+            self.transformCube(face[i], i, centerPoint, gap)
+            cmds.setKeyframe('%s.translateY' % ("cube%s" % face[i]), t=frame)
+            #cmds.setKeyframe('%s.sy' % ("cube%s" % face[i]), t=frame)
+            if i < division*division-1:
+                cmds.setKeyframe('%s.translateY' % ("cube%s" % face[i-1]), t=frame)
+                #cmds.setKeyframe('%s.sy' % ("cube%s" % face[i-1]), t=frame)
             frame += 1
             # if count == size or count == 0:
             #     counter = -1*counter
             count += counter
 
     def deleteStairs(self):
-        for i in range(self.currentSize*self.currentSize):
+        for i in range(self.currentDivision * self.currentDivision):
             cmds.delete('cube%s' % i)
+        self.cubeCreated = False
+
+    def deleteAll(self):
+        for i in range(self.currentDivision * self.currentDivision):
+            cmds.delete('cube%s' % i)
+        self.face = []
+        self.created = False
+        self.cubeCreated = False
+
 
     def saveScreenshot(self, name, directory=DIRECTORY):
         frame = cmds.currentTime(q=True)
         path = os.path.join(directory, '%s[%s].jpg' % (name, frame))
-        for i in range(self.currentSize*self.currentSize):
+        for i in range(self.currentDivision * self.currentDivision):
             cmds.select('cube%s' % i, add=True)
 
         cmds.viewFit()
@@ -151,9 +248,40 @@ class Stairs(object):
         cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
 
         cmds.playblast(completeFilename=path,forceOverwrite=True, format="image", viewer=True, width=480, height=480,
-                       showOrnaments=False)
+                       showOrnaments=False, startTime=frame,endTime=frame)
 
         return path
+
+    def setShader(self):
+        cmds.shadingNode("phong", asShader=True, name="treeTexture")
+        cmds.connectAttr('filePathImage.outColor', 'treeTexture.color')
+        imagePath = 'D:/fly.jpg'
+        cmds.setAttr('filePathImage.fileTextureName', imagePath, type="string")
+
+    def setColour(self):
+        self.colour = (0,0,0)
+
+    def apply_texture(self):
+        object = cmds.ls(sl=True)
+        cmds.optionMenu('optionMenu')
+        selectedMenuItem = cmds.optionMenu('optionMenu', q=True, value=True)
+        cmds.sets(name='imageMaterialGroup', renderable=True, empty=True)
+        shaderNode = cmds.shadingNode('phong', name='shaderNode', asShader=True)
+        fileNode = cmds.shadingNode('file', name='fileTexture', asTexture=True)
+        cmds.setAttr('fileTexture'+'.fileTextureName', self.myDir[0]+'/'+selectedMenuItem, type="string")
+        shadingGroup = cmds.sets(name='textureMaterialGroup', renderable=True, empty=True)
+        cmds.connectAttr('shaderNode'+'.outColor','textureMaterialGroup'+'.surfaceShader')
+        cmds.connectAttr('fileTexture'+'.outColor','shaderNode'+'.color')
+        cmds.surfaceShaderList('shaderNode', add='imageMaterialGroup')
+        cmds.sets(object, e=True, forceElement='imageMaterialGroup')
+
+    def applyMaterial(self, type):
+        for i in range(self.currentDivision * self.currentDivision):
+            node = 'cube%s' % i
+            shd = cmds.shadingNode('%s' % type, name="%s_%s" % (node,type) , asShader=True)
+            shdSG = cmds.sets(name='%sSG' % shd, empty=True, renderable=True, noSurfaceShader=True)
+            cmds.connectAttr('%s.outColor' % shd, '%s.surfaceShader' % shdSG)
+            cmds.sets(node, e=True, forceElement=shdSG)
 
 
 
